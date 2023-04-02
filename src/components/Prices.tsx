420941ab5073833f6bffc21b3febad80a0dd6f19
Ismail Sacic <cicas03@hotmail.com>
2023-04-02 11:45:34 +0200

    Changed Price Page

--- src/components/Prices.tsx
+++ src/components/Prices.tsx
@@ -3,45 +3,45 @@ import { Component,  createResource, createSignal  } from "solid-js";
 const Prices: Component = () => {
     
     const baseUrl = import.meta.env.PUBLIC_URL;
-    const [error, setError] = createSignal(null);
+    const [error, setError] = createSignal("");
 
     const [Prices] = createResource(async () => {
-        const response = await fetch(baseUrl + '/api/prices?populate=*');
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            return data;
+        try{
+            const response = await fetch(baseUrl + '/api/products');
+            if (response.ok) {
+                const data = await response.json();
+                return data;
+            }
         }
-
-        setError(response.statusText);
-        throw new Error('Failed to fetch prices');
+        catch (e) {
+            setError("Failed to fetch prices");
+        }
     });
-
-
 
     return (
         <div class="container" style="margin-top:40px;">
             <span>{Prices.loading && "Loading..."}</span>
             <span>{error() && "Error: " + error()}</span>
             <div class="all_cards">
-                { Prices.loading ? null : Prices().data.map((price :any) => (
+                { Prices.loading ? null : Prices().map((price :any) => (
                     <div class="card">
                         <div class="card-picture">
-                            <img src= {baseUrl + price.attributes.picture.data.attributes.formats.large.url} alt={price.attributes.picture.data.attributes.name} />
+                            <img src= {baseUrl + "/static/uploads/"+ price.image} alt={price.image} />
                         </div>
                         <div class="card-body">
-                            <p class="card-body-name">{price.attributes.name}</p>
-                            { price.attributes.discount ? 
+                            <p class="card-body-name">{price.title}</p>
+                            { price.on_sale ? 
                                 <div class="card-body-discount">
-                                    <p class="card-body-price-original ">{price.attributes.price} kr</p>
-                                    <p class="card-body-discount-price">{price.attributes.discount_price} kr</p> 
+                                    <p class="card-body-price-original ">{price.price} kr</p>
+                                    <p class="card-body-discount-price">{price.sale_price} kr</p> 
                                 </div>
                                 : 
-                                <p class="card-body-price">{price.attributes.price} kr</p>
+                                <p class="card-body-price">{price.price} kr</p>
                             }
                         </div>
                     </div>
                 ))}
+                { Prices.loading ? null : Prices().length === 0 ? <>Det finns inga priser att visa</> : null }
             </div>
         </div>
     );
